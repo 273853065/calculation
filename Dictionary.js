d7fcba80f4a31bc152ae464408b83e980681afeb
lavender <273853065@qq.com>
2015-12-02 14:19:29 +0800

    # add: 修改字典类，添加count和clear方法，Object.keys()理解

--- Dictionary.js
+++ Dictionary.js
@@ -1,21 +1,23 @@
 function Dictionary() {
     this.add = add;
-    this.datastore = new Array();
+    this.dataStore = new Array();
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
 
 function add(key, value) {
-    this.datastore[key] = value;
+    this.dataStore[key] = value;
 }
 
 function find(key) {
-    return this.datastore[key];
+    return this.dataStore[key];
 }
 
 function remove(key) {
-    delete this.datastore[key];
+    delete this.dataStore[key];
 }
 
 //function showAll() {
@@ -25,14 +27,29 @@ function remove(key) {
 //}
 
 function  showAll() {
-    var  keys = Object.keys(this.datastore);
+    var  keys = Object.keys(this.dataStore);
     //将数组this.datastore中不为空的下标返回给数组
     //调用Object.keys后返回的是key数组，然后下面的for in遍历的的key是数组下标
     //返回一数组，其中包含有Dictionary对象的所有现存键。
     //Object.keys() 方法会返回一个由给定对象的所有可枚举自身属性的属性名组成的数组，数组中属性名的排列顺序和使用for-in循环遍历该对象时返回的顺序一致(两者的主要区别是 for-in 还会遍历出一个对象从其原型链上继承到的可枚举属性)。
     //Object.keys(obj)  obj返回该对象的所有可枚举自身属性的属性名。
     //Object.keys 返回一个所有元素为字符串的数组，其元素来自于从给定的对象上面可直接枚举的属性。这些属性的顺序与手动遍历该对象属性时的一致。
-    for (var  index  in  keys) {//index还是从0开始自增的数字        
-        print(keys[index]  +  " -> "  +  this.datastore[keys[index]]);    
+    for (var  index  in  keys) { //index还是从0开始自增的数字        
+        print(keys[index]  +  " -> "  +  this.dataStore[keys[index]]);    
+    }
+}
+
+function count() {//count必须获取下标
+    var n = 0;
+    var keys = Object.keys(this.dataStore);
+    for(var index in keys) {
+        ++n;
+    }
+    return n;
+}
+
+function clear() {//不需要再找出this.dataStore中有键值的项，直接清空this.dataStore数组即可
+    for(var index in this.dataStore) {
+        delete this.dataStore[index];
     }
 }
--- usedict.js
+++ usedict.js
@@ -1,8 +1,18 @@
 load("Dictionary.js");
 var pbook = new Dictionary();
-pbook.add("Mike", "123");
+pbook.add("Raymond", "123");
 pbook.add("David", "345");
-pbook.add("Cynthia", "456");//上面这几步其实是这样的 pbook = [{"Mike":"123"},{"David":"345"},{"Cynthia":"456"}];
+pbook.add("Cynthia", "456");
+print("Number of entries: " + pbook.count());
 print("David's extension: " + pbook.find("David"));
-pbook.remove("David");
 pbook.showAll();
+pbook.clear();
+print("Number of entries: " + pbook.count());
+var nums = new Array();
+nums[0] = 1;
+nums[1] = 2;
+print(nums.length); // 显示 2
+var pbook = new Array();
+pbook["David"] = 1;
+pbook["Jennifer"] = 2;
+print(pbook.length); // 显示 0
