127eb040db85c099e5e3db8b962a63c0d097543e
lavender <273853065@qq.com>
2016-01-05 17:31:56 +0800

    # mod: 图添加最短路径方法

--- graph.js
+++ graph.js
@@ -1,8 +1,8 @@
-function Graph(v){
+function Graph(v) {
     this.vertices = v;
     this.edges = 0;
     this.adj = [];
-    for(var i = 0;i < this.vertices;++ i){
+    for (var i = 0; i < this.vertices; ++i) {
         this.adj[i] = [];
         this.adj[i].push("");
     }
@@ -11,23 +11,27 @@ function Graph(v){
     this.showGraph = showGraph;
     this.dfs = dfs;
     this.marked = [];
-    for(var i = 0;i < this.vertices;++ i){
+    for (var i = 0; i < this.vertices; ++i) {
         this.marked[i] = false;
     }
     this.bfs = bfs;
+    this.pathTo = pathTo;
+    this.hasPathTo = hasPathTo;
+    this.topSortHelper = topSortHelper;
+    this.topSort = topSort;
 }
 
-function addEdge(v,w){
+function addEdge(v, w) {
     this.adj[v].push(w);
     this.adj[w].push(v);
     this.edges++;
 }
 
-function showGraph(){
-    for(var i = 0;i < this.vertices;++ i){
-        putstr( i + " -> ");
-        for(var j = 0;j < this.vertices;++ j){
-            if(this.adj[i][j] !== undefined){
+function showGraph() {
+    for (var i = 0; i < this.vertices; ++i) {
+        putstr(i + " -> ");
+        for (var j = 0; j < this.vertices; ++j) {
+            if (this.adj[i][j] !== undefined) {
                 putstr(this.adj[i][j] + "  ");
             }
         }
@@ -35,13 +39,13 @@ function showGraph(){
     }
 }
 
-function dfs(v){
+function dfs(v) {
     this.marked[v] = true;
-    if(this.adj[v] !== undefined){
+    if (this.adj[v] !== undefined) {
         print("Visited vertex:  " + v);
     }
-    for each(var w in this.adj[v]){
-        if(!this.marked[w]){
+    for each(var w in this.adj[v]) {
+        if (!this.marked[w]) {
             this.dfs(w);
         }
     }
@@ -50,22 +54,68 @@ function dfs(v){
 //(1) 查找与当前顶点相邻的未访问顶点，将其添加到已访问顶点列表及队列中；
 //(2) 从图中取出下一个顶点 v ，添加到已访问的顶点列表；
 //(3) 将所有与 v 相邻的未访问顶点添加到队列。
-function bfs(s){
+this.edgeTo = [];
+
+function bfs(s) {
     var queue = [];
     this.marked[s] = true;
     queue.push(s);
-    while(queue.length > 0){
+    while (queue.length > 0) {
         var v = queue.shift();
-        if(v === undefined){
+        if (v === undefined) {
             print("Visited vertex:  " + v);
         }
-        for each(var w in this.adj[v]){
-            if(!this.marked[w]){
+        for each(var w in this.adj[v]) {
+            if (!this.marked[w]) {
                 this.edgeTo[w] = v;
                 this.marked[w] = true;
                 queue.push(w);
             }
         }
     }
-    
+}
+
+function pathTo(v) {
+    var source = 0;
+    if (!this.hasPathTo(v)) {
+        return undefined;
+    }
+    var path = [];
+    for (var i = v; i !== source; i = this.edgeTo[i]) {
+        path.push(i);
+    }
+    path.push(source);
+    return path;
+}
+
+function hasPathTo(v) {
+    return this.marked[v];
+}
+
+function topSort() {
+    var stack = [];
+    var visited = [];
+    for (var i = 0; i < this.vertices; i++) {
+        visited[i] = false;
+    }
+    for (var i = 0; i < this.vertices; i++) {
+        if (visited[i] == false) {
+            this.topSortHelper(i, visited, stack);
+        }
+    }
+    for (var i = 0; i < stack.length; i++) {
+        if (stack[i] != undefined && stack[i] != false) {
+            print(this.vertexList[stack[i]]);
+        }
+    }
+}
+
+function topSortHelper(v, visited, stack) {
+    visited[v] = true;
+    for each(var w in this.adj[v]) {
+        if (!visited[w]) {
+            this.topSortHelper(visited[w], visited, stack);
+        }
+    }
+    stack.push(v);
 }
